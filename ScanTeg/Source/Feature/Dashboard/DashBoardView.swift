1ac54336bf26d101193ef2744019824648183025
Jithin Balan <jithinpala@gmail.com>
2026-02-09 20:47:21 +1100

    JB1 - Basic project setup
    Setup folder strecture

---
+++ ScanTeg/Source/Feature/Dashboard/DashBoardView.swift
@@ -0,0 +1,24 @@
+//
+//  DashBoardView.swift
+//  ScanTeg
+//
+//  Created by Jithin Balan on 9/2/2026.
+//
+
+import SwiftUI
+
+struct DashBoardView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DashBoardView()
+}
